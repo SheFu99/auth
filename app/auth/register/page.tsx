61246aa4fecd31b34997a6436aebfe923bffe238
SheFu99 <94874784+SheFu99@users.noreply.github.com>
2024-04-08 14:41:02 +0200

    08.04 fix 3

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,10 +1,14 @@
+import React, { Suspense } from "react";
 
-import RegisterForm from "@/components/auth/registerform";
+  
+const RegisterForm = React.lazy(() => import ("@/components/auth/registerform"))
 
 const RegisterPage = () => {
     return ( 
         <div>
+         <Suspense fallback={<div>Loading...</div>}>
            <RegisterForm></RegisterForm>
+           </Suspense>
         </div> 
      );
    
