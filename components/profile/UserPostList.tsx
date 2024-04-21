2f858ca70cb7563dbf90bd5bc3986830c86b93e3
SheFu99 <94874784+SheFu99@users.noreply.github.com>
2024-04-21 15:37:36 +0200

    21.04 grid post image MVP

--- components/profile/UserPostList.tsx
+++ components/profile/UserPostList.tsx
@@ -245,9 +245,9 @@ const user = useCurrentUser()
                     {user?.id === post.userId&&(
                         <button title="delete post" key={index} className="text-black" onClick={(e)=>deletePost(post.PostId)}><RiDeleteBin5Line color="black" className="scale-110  absolute top-2 right-2"/> </button>
                     )}
-                        <div key={index}>
+                        {/* <div key={index}>
                             <ImageGrid images={post.image} />
-                        </div>
+                        </div> */}
                     {/* <small>{post.timestamp.tolocalString()}</small> */}
                     <div className="flex">
                         <button title="like" className="text-black" onClick={() => like(post.PostId)} disabled={isPending}>
