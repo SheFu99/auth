9ff83c69d1e9862d87f84b103d7edc1667f2de86
SheFu99 <94874784+SheFu99@users.noreply.github.com>
2024-05-04 17:34:44 +0200

    04.05 image lightbox

--- components/profile/post/Image-grid.tsx
+++ components/profile/post/Image-grid.tsx
@@ -1,53 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
-import Lightbox from './Light-box';
+import Lightbox from "yet-another-react-lightbox";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+
+import "yet-another-react-lightbox/styles.css";
+
+
 export default  function ImageGrid( {images} ) {
     const [isOpen, setIsOpen] = useState(false);
-    const [selectedImg, setSelectedImg] = useState();
     const [imageState , setImageState]= useState<number>()
 
-   
-    const openLightbox = (index,img) => {
+    const modifyiedArray= images.map((url)=>{
+        return{src:url.url}
+    })
+
+   const animationDuration = 500
+   const maxZoomPixelRatio=2
+   const zoomInMultiplier=2
+   const doubleTapDelay=300
+   const doubleClickDelay=300
+   const doubleClickMaxStops=2
+   const keyboardMoveDistance=50
+   const wheelZoomDistanceFactor=100
+   const pinchZoomDistanceFactor = 100
+   const scrollToZoom =true
+
+    const openLightbox = (index) => {
         setIsOpen(true);
         setImageState(index)
-        console.log(images.length,imageState)
-        setSelectedImg(img)
-    };
-useEffect(()=>{
-console.log(imageState)
-},[imageState])
-    const gotoPrevious = () =>{
-        console.log(imageState)
-        if(imageState -1 >= 0 ){
-            console.log(imageState -1)
-        imageState > 0 && setImageState(imageState - 1);
-        const filteredImageByIndex = images.filter((img,i)=> i === imageState-1)
-     
-        const urlInArray = filteredImageByIndex[0].url
-        setSelectedImg(urlInArray)
-    }
-    console.log('begin!')
     };
 
-    const gotoNext = () =>{
-
-          if(imageState  +1< images.length ){
-            console.log(imageState +1)
-            setImageState(imageState + 1);
-            const filteredImageByIndex = images.filter((img,i)=> i === imageState+1)
-            const urlInArray =filteredImageByIndex[0].url
-            setSelectedImg(urlInArray)
-            
-        }
-        console.log('end!')
-    };
-
-    const onClose = ()=>{
-        console.log("CLOSE")
-        setIsOpen(false)
-        setImageState(null)
-    }
-    
 
     const getGridForImage = (index) => {
         switch (images.length) {
@@ -60,11 +42,11 @@ console.log(imageState)
             default: return "w-full h-full";
         }
     };
-    console.log(selectedImg)
+
     return (
         <div className={`grid grid-cols-6  gap-2  ${images.length === 0 ? 'grid-cols-1' : ''}`}>
             {images?.map((img, index) => (
-                <div key={index} className={`${getGridForImage(index)} relative cursor-pointer col-span-1 `} onClick={() => openLightbox(index,img.url)}>
+                <div key={index} className={`${getGridForImage(index)} relative cursor-pointer col-span-1 `} onClick={() => openLightbox(index)}>
                     <Image
                         key={index}
                         src={img.url}
@@ -79,20 +61,28 @@ console.log(imageState)
                 
             ))}
              
-            {/* {isOpen && (
-                <div className=" inset-0 absolute bg-black bg-opacity-75 flex justify-center items-center z-50 h-[90vh] w-[90vh]" onClick={() => setIsOpen(false)}>
-                    <img src={selectedImg} alt="Lightbox img" className="max-h-full max-w-full " />
-                </div>
-            )} */}
-            <Lightbox isOpen={isOpen}
-             onClose={onClose} 
-             images={selectedImg} 
-             goToNext={gotoNext} 
-             goToPrevious={gotoPrevious}
-             
-           
-             />
+
+         
+               <Lightbox
+                open={isOpen}
+                close={() => setIsOpen(false)}
+                index={imageState}
+                slides={modifyiedArray}
+                plugins={[Zoom]}
+                animation={{ zoom: animationDuration }}
+                zoom={{
+                maxZoomPixelRatio,
+                zoomInMultiplier,
+                doubleTapDelay,
+                doubleClickDelay,
+                doubleClickMaxStops,
+                keyboardMoveDistance,
+                wheelZoomDistanceFactor,
+                pinchZoomDistanceFactor,
+                scrollToZoom,
+                }}
                 
+            />  
              
              
         </div>
