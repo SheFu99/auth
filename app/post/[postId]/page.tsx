8224fd756b8aa6ca638e0bcb00ab493311c8a627
SheFu99 <94874784+SheFu99@users.noreply.github.com>
2024-06-08 16:50:15 +0200

    08.06 prefetch and hydration + SEO head

--- app/post/[postId]/page.tsx
+++ app/post/[postId]/page.tsx
@@ -2,19 +2,19 @@
 
 import { getCommentByPost, getPostById } from "@/actions/post";
 import { auth } from "@/auth";
-import { usePost } from "@/components/profile/post/lib/usePost";
 import PostCard from "@/components/profile/post/postCard/PostCard";
 import InfiniteCommentList from "@/components/profile/post/postCard/lists/InfiniteCommentList";
 import queryClientConfig from "@/lib/QueryClient";
 import { prefetchCommentList, prefetchPost } from "@/lib/prefetchQuery";
-import QueryProvider from "@/util/QueryProvider";
 import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
+import Head from "next/head";
 
 const PostPage = async ({params}) => {
     const postId = params?.postId
     await prefetchPost(postId)
     await prefetchCommentList(postId)
-    const post = await getPostById(postId)
+    const data = await getPostById(postId)
+    const post= data?.post
     const dehydratedState = dehydrate(queryClientConfig)
     const comments = await getCommentByPost({PostId:postId,page:1})
 
@@ -23,14 +23,27 @@ const PostPage = async ({params}) => {
 
     return ( 
         <HydrationBoundary state={dehydratedState}>
+            <>
+            {post&&(
+                <Head>
+                <title>{post.user.name} - Post</title>
+                <meta name="description" content={post.superText} />
+                <meta property="og:title" content={post.user.name} />
+                <meta property="og:description" content={post.text} />
+                <meta property="og:image" content={post?.image[0]?.url||''} />
+              </Head>
+            )}
+            
             <div>
-                {post?.post&&(
+            
+                {post&&(
                     <div className="border min-h-[85vh]">
                     <PostCard postId={postId} currentUser={user} />
                     <InfiniteCommentList postId={postId} user={user} commentsCount={post.post?._count?.comments}/>
                     </div>
                 )}
             </div>
+            </>
         </HydrationBoundary>
      );
 }
